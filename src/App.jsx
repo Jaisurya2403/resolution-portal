c84da64828ab2f8b4070edd49c53f842b39c9083
jai <jaisurya7482@gmal.com>
2026-08-05 20:24:18 +0530

    signup page created

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Welcome from './components/Welcome'
 import HomePage from './components/HomePage'
 import Login from './components/Login'
+import Signup from './components/Signup'
 function App() {
   return (
     <div>
+      <Signup/>
       <Login/>
     </div>
   )
